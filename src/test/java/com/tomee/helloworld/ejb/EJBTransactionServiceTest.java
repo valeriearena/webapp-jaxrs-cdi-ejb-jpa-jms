10f4c6ee35a805c9f7488a12448b201138fcae37
valeriearena <arena.valerie@gmail.com>
2016-01-13 13:00:52 -0500

    added unit tests

---
+++ src/test/java/com/tomee/helloworld/ejb/EJBTransactionServiceTest.java
@@ -0,0 +1,66 @@
+package com.tomee.helloworld.ejb;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by valerie on 1/13/16.
+ */
+public class EJBTransactionServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testAddGreeting() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateGreeting() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGreeting() throws Exception {
+
+    }
+
+    @Test
+    public void testAddUser() throws Exception {
+
+    }
+
+    @Test
+    public void testAddUserCascadePersist() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateUserCascadeUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+    }
+}
---
+++ src/test/java/com/tomee/helloworld/jpa/dao/JPASalutationDAOTest.java
@@ -0,0 +1,51 @@
+package com.tomee.helloworld.jpa.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by valerie on 1/13/16.
+ */
+public class JPASalutationDAOTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testPersist() throws Exception {
+
+    }
+
+    @Test
+    public void testFind() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEntityManager() throws Exception {
+
+    }
+
+    @Test
+    public void testSetEntityManager() throws Exception {
+
+    }
+}
